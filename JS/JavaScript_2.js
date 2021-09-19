3cccad1a30c53b520996022f75686ce54b294ab4
CircadiaStC <transparentcircadia@gmail.com>
2021-09-18 18:48:51 -0700

    overhaul of portfolio

--- JS/JavaScript_2.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    let x = document.forms["contactForm"]["fname"].value;
-    if (x == "")    {
-        alert("Please provide a name");
-        return false;
-    }  
-    
- }
